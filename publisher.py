fc1cc2d0745086e80fd7ec351a56d5fa512f475d
joebui <dienbuijoe@outlook.com>
2017-08-26 11:44:05 +0700

    edit python file

--- publisher.py
+++ publisher.py
@@ -4,4 +4,5 @@ broker_address = '172.16.123.84'
 
 client = mqtt.Client("P1")
 client.connect(broker_address)
-client.publish('station', 'OFF')
+client.publish('lora_gps', 'lora')
+client.publish('rfid', 'rfid')
